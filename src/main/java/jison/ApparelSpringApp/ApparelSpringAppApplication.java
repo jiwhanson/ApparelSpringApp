224601830fa518e153f08aa6afc5d0224b55f221
Jiwhan Son <jiwhanson@utexas.edu>
2020-08-11 17:27:46 -0500

    Spring Initializr Project

---
+++ src/main/java/jison/ApparelSpringApp/ApparelSpringAppApplication.java
@@ -0,0 +1,13 @@
+package jison.ApparelSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparelSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApparelSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jison/ApparelSpringApp/ApparelSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jison.ApparelSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApparelSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
